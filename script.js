ed687070ac04266ff15e583313cb593b229bb8aa
tyagi-divyansh <tyagidipanshu098@gmail.com>
2023-07-22 20:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openMenu(){
   document.querySelector(".menu").style.right=0;
-
+  document.querySelector(".fa-bars").style.opacity=
+  "0";
 }
 function closeMenu(){
   document.querySelector(".menu").style.right="-400px";
-
+  document.querySelector(".fa-bars").style.opacity=
+  "100%";
 }
 
